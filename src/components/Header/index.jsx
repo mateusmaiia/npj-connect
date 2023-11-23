7a78f206b8fc9dc0a095735be5e95eca16480ae8
famgz <famgz@proton.me>
2023-11-23 18:33:28 -0300

    refactor: navbar mobile menu icon

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,45 +1,43 @@
 import './stylevideo.css'
 import Logo from '../../assets/npj_com_borda_1.svg'
 import UserIcon from '../../assets/mdi_user.svg'
+import MenuIcon from '../../assets/menu.svg'
 
 
-export function Header(){
-    return(
+export function Header() {
+    return (
         <header>
-            
+
             <nav >
                 <div className='logo'>
                     <a href="">
-                        <img 
+                        <img
                             className='logo-icon'
-                            src={Logo} 
-                            alt="Logo" 
+                            src={Logo}
+                            alt="Logo"
                         />
-                    </a>    
-                   
-                </div>  
+                    </a>
+
+                </div>
                 <div className='mobile-menu'>
-                    <div className='line1'></div>
-                    <div className='line2'></div>
-                    <div className='line3'></div>
-                    <div className='line4'></div>
+                    <img src={MenuIcon} alt='Menu Icon'/>
                 </div>
                 <ul className='nav-list'>
-                
+
                     <li><a href="#">Sobre</a></li>
                     <li><a href="#">Quem somos?</a> </li>
                     <li><a href="#">O que fazemos?</a></li>
                     <li><a href="#">Perguntas Frequentes</a></li>
-                <a href="/" className='icon'>
-                    <img 
-                        className='user-icon'
-                        src={UserIcon} 
-                        alt="" 
+                    <a href="/" className='icon'>
+                        <img
+                            className='user-icon'
+                            src={UserIcon}
+                            alt=""
                         />
-                </a>
+                    </a>
                 </ul>
             </nav>
-            
+
         </header>
     )
 }
