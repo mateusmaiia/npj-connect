10765befcf816dbfd738e7b5cdaa06737379b7eb
maia <matteusmaia889@gmail.com>
2023-11-24 18:07:10 -0300

    modal

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,33 @@
+<<<<<<< HEAD
 import './stylevideo.css'
 import Logo from '../../assets/npj_com_borda_1.svg'
 import UserIcon from '../../assets/mdi_user.svg'
 import MenuIcon from '../../assets/menu.svg'
+=======
+import './stylevideo.css';
+import Logo from '../../assets/npj_com_borda_1.svg';
+import UserIcon from '../../assets/mdi_user.svg';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+>>>>>>> 16edde0 (modal)
 
+const modalStyle = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 export function Header() {
+<<<<<<< HEAD
 
     function handleMenuClick() {
         console.log('handleMenu')
@@ -41,3 +64,70 @@ export function Header() {
         </header>
     )
 }
+=======
+  const [isModalOpen, setModalOpen] = React.useState(false);
+
+  const handleOpenModal = (event) => {
+    event.preventDefault()
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <header>
+      <nav>
+        <div className='logo'>
+          <a href=''>
+            <img className='logo-icon' src={Logo} alt='Logo' />
+          </a>
+        </div>
+        <div className='mobile-menu'>
+          <div className='line1'></div>
+          <div className='line2'></div>
+          <div className='line3'></div>
+          <div className='line4'></div>
+        </div>
+        <ul className='nav-list'>
+          <li>
+            <a href='#'>Sobre</a>
+          </li>
+          <li>
+            <a href='#'>Quem somos?</a>{' '}
+          </li>
+          <li>
+            <a href='#'>O que fazemos?</a>
+          </li>
+          <li>
+            <a href='#'>Perguntas Frequentes</a>
+          </li>
+          <a href='/' className='icon' onClick={handleOpenModal}>
+            <img className='user-icon' src={UserIcon} alt='' />
+          </a>
+        </ul>
+        <Modal
+          open={isModalOpen}
+          onClose={handleCloseModal}
+          aria-labelledby='modal-modal-title'
+          aria-describedby='modal-modal-description'
+          
+        >
+          <Box style={modalStyle} className='custom-modal'>
+            <Typography id='modal-modal-title' variant='h6' component='h2' className='title-modal'>
+              CONECTE-SE
+            </Typography>
+
+            <Button className='button-modal'>CADASTRAR</Button>
+
+            <Typography id='modal-modal-description' className='possuiconta' sx={{ mt: 2 }}>
+              Já possui conta? <span className='entrar'>Entrar</span>
+            </Typography>
+          </Box>
+        </Modal>
+      </nav>
+    </header>
+  );
+}
+>>>>>>> 16edde0 (modal)
