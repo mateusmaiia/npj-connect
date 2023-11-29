f922c3fe01c23ffb0002d2db2b74b1988e78222f
famgz <famgz@proton.me>
2023-11-28 22:01:59 -0300

    feat: add MainPage FAQ dropdown buttons

---
+++ src/components/FAQDropDown/index.jsx
@@ -0,0 +1,73 @@
+import './style.css'
+
+
+import Dropdown from 'react-bootstrap/Dropdown';
+// import DropdownButton from 'react-bootstrap/DropdownButton';
+
+function ButtonDarkExample() {
+  return (
+    <>
+      <div className="dropdowns">
+
+        <Dropdown>
+          <Dropdown.Toggle variant="">
+            O serviço é gratuito?
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio earum in veritatis, ipsa tempora laborum cum sint et perferendis dolor exercitationem, ea blanditiis qui enim voluptates. Ratione adipisci reprehenderit doloremque!
+              Deserunt odit ipsum amet exercitationem! Alias quidem inventore similique tempora nobis quibusdam, nostrum odit, facere repellat at consequuntur vitae. Iure animi nulla fuga iste? Tenetur dignissimos saepe sapiente autem quasi?
+              Veritatis architecto, laborum sunt recusandae id corrupti officiis accusamus officia eius fuga alias similique non voluptatum veniam sint quam quia nam ad distinctio, repellendus nostrum ipsam. Sunt animi odit at?
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <Dropdown>
+          <Dropdown.Toggle variant="">
+          O serviço é seguro?
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio earum in veritatis, ipsa tempora laborum cum sint et perferendis dolor exercitationem, ea blanditiis qui enim voluptates. Ratione adipisci reprehenderit doloremque!
+              Deserunt odit ipsum amet exercitationem! Alias quidem inventore similique tempora nobis quibusdam, nostrum odit, facere repellat at consequuntur vitae. Iure animi nulla fuga iste? Tenetur dignissimos saepe sapiente autem quasi?
+              Veritatis architecto, laborum sunt recusandae id corrupti officiis accusamus officia eius fuga alias similique non voluptatum veniam sint quam quia nam ad distinctio, repellendus nostrum ipsam. Sunt animi odit at?
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <Dropdown>
+          <Dropdown.Toggle variant="">
+            Preciso ser associado à instituição?
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio earum in veritatis, ipsa tempora laborum cum sint et perferendis dolor exercitationem, ea blanditiis qui enim voluptates. Ratione adipisci reprehenderit doloremque!
+              Deserunt odit ipsum amet exercitationem! Alias quidem inventore similique tempora nobis quibusdam, nostrum odit, facere repellat at consequuntur vitae. Iure animi nulla fuga iste? Tenetur dignissimos saepe sapiente autem quasi?
+              Veritatis architecto, laborum sunt recusandae id corrupti officiis accusamus officia eius fuga alias similique non voluptatum veniam sint quam quia nam ad distinctio, repellendus nostrum ipsam. Sunt animi odit at?
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <Dropdown>
+          <Dropdown.Toggle variant="">
+            Posso escolher o advogado?
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio earum in veritatis, ipsa tempora laborum cum sint et perferendis dolor exercitationem, ea blanditiis qui enim voluptates. Ratione adipisci reprehenderit doloremque!
+              Deserunt odit ipsum amet exercitationem! Alias quidem inventore similique tempora nobis quibusdam, nostrum odit, facere repellat at consequuntur vitae. Iure animi nulla fuga iste? Tenetur dignissimos saepe sapiente autem quasi?
+              Veritatis architecto, laborum sunt recusandae id corrupti officiis accusamus officia eius fuga alias similique non voluptatum veniam sint quam quia nam ad distinctio, repellendus nostrum ipsam. Sunt animi odit at?
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <Dropdown>
+          <Dropdown.Toggle variant="">
+            Terei garantia de atendimento?
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio earum in veritatis, ipsa tempora laborum cum sint et perferendis dolor exercitationem, ea blanditiis qui enim voluptates. Ratione adipisci reprehenderit doloremque!
+              Deserunt odit ipsum amet exercitationem! Alias quidem inventore similique tempora nobis quibusdam, nostrum odit, facere repellat at consequuntur vitae. Iure animi nulla fuga iste? Tenetur dignissimos saepe sapiente autem quasi?
+              Veritatis architecto, laborum sunt recusandae id corrupti officiis accusamus officia eius fuga alias similique non voluptatum veniam sint quam quia nam ad distinctio, repellendus nostrum ipsam. Sunt animi odit at?
+          </Dropdown.Menu>
+        </Dropdown>
+
+      </div>
+
+    </>
+  );
+}
+
+export default ButtonDarkExample;
